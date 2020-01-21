4e172ccb5fa8d4093eec4511a27dee9f9fa3a7d1
Matias Quevedo <matiasquevedo.sabbath@gmail.com>
2020-01-21 17:11:53 -0300

    elimino link staticos

--- resources/views/layouts/nav.blade.php
+++ resources/views/layouts/nav.blade.php
@@ -17,9 +17,9 @@
               @if(count(\App\Album::where('state','1')->get())>0)
                 <li class="nav-item"><a class="nav-link" href="">Galeria</a></li>
               @endif
-              @foreach($pages as $page)
+              {{-- @foreach($pages as $page)
                 <li class="nav-item"><a class="nav-link" href="{{route('public.page',$page->slug)}}">{{ ucwords($page->title) }}</a></li>
-              @endforeach
+              @endforeach --}}
 
               @if($public_menu)
                   @foreach($public_menu as $menu)
